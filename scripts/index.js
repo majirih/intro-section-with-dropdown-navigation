bc5bc0d8d451f247ecde6fc76500ce520265d5f3
majirih <86539930+majirih@users.noreply.github.com>
2022-12-24 01:41:22 +0100

    added image to features, styled learn-more button

--- scripts/index.js
+++ scripts/index.js
@@ -16,13 +16,13 @@ function myFunction() {
 function toggleDropdown() {
     document.querySelector("#myDropdown2").classList.toggle("show");
    
-    if (!event.target.matches(".companybtn")) {
-      const dropdown = document.querySelector(".company-content");
+    if (!event.target.matches(".dropbtn")) {
+      const dropdowns = document.querySelector(".dropdown-content");
       let i;
-      for (i = 0; i < dropdown.length; i++) {
-        const openDropdowns = dropdown[i];
-        if (openDropdowns.classList.contains("show")) {
-          openDropdowns.classList.remove("show");
+      for (i = 0; i < dropdowns.length; i++) {
+        const openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains("show")) {
+          openDropdown.classList.remove("show");
         }
       }
     }
